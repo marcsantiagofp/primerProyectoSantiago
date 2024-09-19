8f9111aa621bb04b2cefc6defe225fc663451c62
Marc Santiago <marcsantiagofp@ibf.cat>
2024-09-19 18:55:33 +0200

    new file:   primerProyectoSantiago.php

---
+++ primerProyectoSantiago.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marc Santiago</title>
+</head>
+<body>
+    <h1>Bienvenido a primerProyectoSantiago.com</h1>
+    <?php
+            echo "Esto es una prueba";
+    ?>
+</body>
+</html>
